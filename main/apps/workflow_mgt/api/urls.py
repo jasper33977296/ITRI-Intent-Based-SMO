70d68c4d7c3526d2e39a78f90d94b8253951cbd1
yihua.cai135 <jess92725@gmail.com>
2025-06-20 07:03:09 +0000

    add:write log function

--- main/apps/workflow_mgt/api/urls.py
+++ main/apps/workflow_mgt/api/urls.py
@@ -3,4 +3,5 @@ from main.apps.workflow_mgt.actors.WorkflowManager import WorkflowManager
 urlpatterns = [
     path("workflow_mgt/WorkflowManager/execute_workflow",WorkflowManager.execute_workflow,name='execute_workflow'),
     path("workflow_mgt/WorkflowManager/human_in_the_loop",WorkflowManager.human_in_the_loop,name='human_in_the_loop'),
+    path("workflow_mgt/WorkflowManager/logger_human_in_the_loop",WorkflowManager.logger_human_in_the_loop,name='logger_human_in_the_loop'),
 ]
