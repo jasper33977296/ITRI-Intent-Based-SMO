44d76e6afd30f437480a122931f5a369ba97f8bd
yihua.cai135 <jess92725@gmail.com>
2025-07-12 00:52:37 +0800

    fix: Update update_workflow_status()

--- main/apps/workflow_mgt/actors/WorkflowManager.py
+++ main/apps/workflow_mgt/actors/WorkflowManager.py
@@ -43,6 +43,27 @@ class WorkflowManager:
                 user_content = text_content[0].get("content", "") 
             else:
                 user_content = "" # 或自行定義預設行為 
+                
+            # 更新 workflow status 1
+            work_payload = {
+                "conversation_uid": conversation_uid,
+                "workflow_status": "1"
+            }
+
+            try:
+                resp = json_request(
+                    module="workflow_mgt",
+                    actor="WorkflowManager",
+                    function="update_workflow_status",
+                    payload=work_payload,
+                )
+                work_data = resp.json()
+            except Exception as e:
+                print("Workflow 更新失敗: ", e)
+
+            # 檢查後端回傳是否成功
+            if not work_data.get("status_code", 400):
+                return JsonResponse(work_data, status_code=work_data.get("status_code", 400))
 
             # (2) 呼叫 dify 執行工作流
             result = dify_single_intent_workflow(conversation_uid, user_content)
@@ -112,23 +133,13 @@ class WorkflowManager:
                     "message": f"Fail to call metadata_mgt API (get_workflow_metadata): {str(e)}"
                 }, status=502)
 
-            # 檢查後端回傳是否成功 ==還需要更改==
+            # 檢查後端回傳是否成功
             if not meta_data.get("status", False):
-                return JsonResponse(meta_data, status=meta_data.get("status_code", 400))
-
-            # 從回傳資料中取出 step, status
-            workflow_info = meta_data.get("data", {})
-            workflow_step = workflow_info.get("workflow_step", "demo")
-
-            # (3) 根據 step 呼叫對應函式
-            result = dify_single_intent_workflow(conversation_uid=conversation_uid,user_prompt=content)
-
-            text_data = result.get("parsed_data","")
+                return JsonResponse(meta_data, status_code=meta_data.get("status_code", 400))
 
             return JsonResponse({
-                    "event_type": workflow_step,
-                    "conversation_uid": conversation_uid,
-                    "text": text_data
+                "status_code": 200,
+                "message": "Workflow 更新成功"
             })
 
         except Exception as e:
